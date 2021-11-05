7778ebaafb821e977002da67f18f9a00e0ef2896
deckedland <2861907705@qq.com>
2021-11-05 17:06:18 +0800

    demo1

--- script.js
+++ script.js
@@ -26,12 +26,38 @@ window.onscroll = function() {
     scrollFunction()
 };
 function scrollFunction() {
-    if (document.documentElement.scrollTop > 300) {
+    if (document.documentElement.scrollTop > 1) {
         document.getElementById("scrollDown").style.display = "none";
     } 
     else {
         document.getElementById("scrollDown").style.display = "block";
     }
+}
+
+// -------------------- In-Out -----------------------
+
+window.onload = function() {
+	const cons = document.querySelectorAll('.con');
+	window.addEventListener("scroll",checkInOut);
+	function checkInOut() {
+		cons.forEach(con=> {
+			const offSetBottom = window.innerHeight + window.scrollY;
+            const offSet = window.scrollY;                 
+			const diversion1 = con.offsetTop;                   
+            const diversion2 = con.offsetTop + con.height/3;    
+            const diversion3 = con.offsetTop + 2*con.height/3;     
+            const diversion4 = con.offsetTop + con.height;         
+			if (offSetBottom>=diversion1 && offSetBottom<diversion2 || offSet>=diversion3 && offSet<diversion4) {
+			    con.classList.replace("active1");
+			}
+			else if (offSetBottom>=diversion2 && offSetBottom<diversion3 || offSet>=diversion2 && offSet<diversion3) {
+			    con.classList.replace("active2");
+            }
+            else if (offSetBottom>=diversion3 && offSetBottom<diversion4 || offSet>=diversion1 && offSet<diversion2) {
+                con.classList.replace("active3");
+            }
+        });
+	}
 }
 
 // -------------------- Sildes -----------------------
@@ -52,5 +78,5 @@ function showSlides() {
     }
     slides[slideIndex-1].style.display = " block";  
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); 
+    setTimeout(showSlides, 2500); 
 }
